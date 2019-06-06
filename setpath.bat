57fe7eeb357b1a6032fc0527d68542c65a306b02
Samru5 <samruddhi-dinesh.shetye@capgemini.com>
2019-06-06 19:20:06 +0530

    add

---
+++ setpath.bat
@@ -0,0 +1,11 @@
+set JAVA_HOME=%CD%\..\software\jdk1.8.0_31
+REM set HADOOP_HOME=%CD%\..\software\hadoop;%CD%\..\software\hadoop\bin
+set PATH=%PATH%;%CD%\..\software\jdk1.8.0_31\bin;%CD%\..\software\scala\sbt\sbt-0.13.9\bin;%CD%\..\software\scala\scala-2.11.7\bin;
+set CLASSPATH=%CD%\..\software\jdk1.8.0_31\lib;%CD%\..\software\scala\scala-2.11.7\lib;%CLASSPATH%;
+
+
+
+
+
+
+
